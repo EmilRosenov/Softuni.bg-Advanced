515c196c0198d0821daedb05ccb1a9c5c294d305
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-11-05 14:55:18 +0000

    Add files via upload

---
+++ 01.StacsAndQueuesLab/02.stackSum/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.stackSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumbers = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+            Stack<int> stckNumbers = new Stack<int>(inputNumbers);
+
+            string[] commandInput = Console.ReadLine()
+                              .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            while (commandInput[0].ToUpper()!="END")
+            {
+                string command = commandInput[0];
+                if (command.ToUpper()=="ADD")
+                {
+                    int first = int.Parse(commandInput[1]);
+                    int second = int.Parse(commandInput[2]);
+
+                    stckNumbers.Push(first);
+                    stckNumbers.Push(second);
+                }
+                else if (command.ToUpper() == "REMOVE")
+                {
+                    int countElementsToRemove = int.Parse(commandInput[1]);
+                    if (countElementsToRemove > stckNumbers.Count)
+                    {
+                        commandInput = Console.ReadLine()
+                              .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                        continue;
+                    }
+                    else
+                    {
+                        while (countElementsToRemove > 0)
+                        {
+                            stckNumbers.Pop();
+                            countElementsToRemove--;
+                        }
+                       
+                    }
+
+                }
+                commandInput = Console.ReadLine()
+                              .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            Console.WriteLine($"Sum: {stckNumbers.Sum()}");
+        }
+    }
+}
