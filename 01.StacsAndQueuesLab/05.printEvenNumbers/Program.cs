9cb3b9dfd9cbe952b2f5e8f24242bb3721b7de05
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-11-05 14:57:00 +0000

    Add files via upload

---
+++ 01.StacsAndQueuesLab/05.printEvenNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.printEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>(numbers);
+            // Queue<int> evenNumbersQueue = new Queue<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (queue.Peek() %2 !=0)
+                {
+                    queue.Dequeue();
+                }
+                else
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+            }
+          
+            Console.Write(string.Join(", ", queue));
+            Console.WriteLine();
+        }
+    }
+}
