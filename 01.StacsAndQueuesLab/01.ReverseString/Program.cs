80211fd9a19154109fc384aabccd6a2cf940b04e
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-11-05 14:54:24 +0000

    Add files via upload

---
+++ 01.StacsAndQueuesLab/01.ReverseString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace stacksAndQueuesLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                stack.Push(text[i]);
+            }
+            while (stack.Count>0)
+            {
+                Console.Write(string.Join("", stack.Pop()));
+            }
+            
+        }
+    }
+}
