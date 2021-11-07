430bcd7aed9eb391f72d604fc53cdb533228d3bc
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-11-07 22:27:39 +0000

    Add files via upload

---
+++ 01.StacsAndQueuesLab/06.supermarket/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputInfo = Console.ReadLine();
+            Queue<string> queue = new Queue<string>();
+            while (inputInfo != "End")
+            {
+
+                if (inputInfo == "Paid")
+                {
+                    while (queue.Count > 0)
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(inputInfo);
+                }
+                inputInfo = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
