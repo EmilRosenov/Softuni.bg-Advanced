b8680cd8033bcd1394fc6407abaf1b48d1359c1b
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-11-07 22:28:09 +0000

    Add files via upload

---
+++ 01.StacsAndQueuesLab/07.hotPotato/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.hotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kids = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> game = new Queue<string>(kids);
+
+            while (game.Count > 1)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    game.Enqueue(game.Dequeue());
+                    
+                }
+                Console.WriteLine($"Removed {game.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {game.Dequeue()}");
+        }
+    }
+}
