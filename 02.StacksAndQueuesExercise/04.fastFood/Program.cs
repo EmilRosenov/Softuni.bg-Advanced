6de6b1de7a40a3e69a9a1a0b65b4ee8d83fc764f
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-11-12 04:05:31 +0000

    Add files via upload

---
+++ 02.StacksAndQueuesExercise/04.fastFood/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.fastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityPrepared = int.Parse(Console.ReadLine());
+            int[] orders = Console.ReadLine()
+                        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+            Queue<int> ordersQueue = new Queue<int>(orders);
+
+            int biggestOrder = ordersQueue.Max();
+
+            Console.WriteLine(biggestOrder);
+
+            for (int i = 0; i < orders.Length; i++)
+            {
+                if (quantityPrepared >= ordersQueue.Peek())
+                {
+                    quantityPrepared -= ordersQueue.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (ordersQueue.Count>0)
+            {
+                Console.WriteLine("Orders left: " + $"{string.Join(" ", ordersQueue)}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+
+        }
+    }
+}
