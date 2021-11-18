b67e5373cde16d07263d0ff51b1f7c8d42e5cd1c
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-11-18 18:02:32 +0000

    Add files via upload

---
+++ 02.StacksAndQueuesExercise/08.balancedParenthesis100/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.balancedParenthesis100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+            foreach (var symbol in input)
+            {
+                if (stack.Any())
+                {
+                    char symbolInList = stack.Peek();
+                    if (symbolInList == '(' && symbol == ')')
+                    {
+                        stack.Pop();
+                        continue;
+                    }
+                    else if (symbolInList == '{' && symbol == '}')
+                    {
+                        stack.Pop();
+                        continue;
+                    }
+                    else if (symbolInList == '[' && symbol == ']')
+                    {
+                        stack.Pop();
+                        continue;
+                    }
+                }
+                stack.Push(symbol);
+            }
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
