c97b330c5e10e8af0b47ceb88dcdc148295749ca
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-11-24 11:15:12 +0000

    Add files via upload

---
+++ 03.MultiDimensionalArraysLab/01.SumMatrix/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = ReadArrayFromConsole();
+
+            int[,] matrix = new int[input[0], input[1]];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] array = ReadArrayFromConsole();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = array[j];
+                }
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            int sum = 0;
+            foreach (int element in matrix)
+            {
+                sum += element;
+            }
+            Console.WriteLine(sum);
+
+            static int[] ReadArrayFromConsole()
+            {
+                return Console.ReadLine()
+                        .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+            }
+        }
+    }
+}
