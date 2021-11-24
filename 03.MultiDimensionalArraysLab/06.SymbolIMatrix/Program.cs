aeeb06b2b8eb5108010627572f977413a9d54a64
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-11-24 11:18:00 +0000

    Add files via upload

---
+++ 03.MultiDimensionalArraysLab/06.SymbolIMatrix/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.SymbolIMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+            int bestRow = int.MinValue;
+            int bestCol = int.MinValue;
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                string input = Console.ReadLine();
+
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    char element = char.Parse(input[cols].ToString());
+                    matrix[rows, cols] = element;
+                }
+            }
+
+            char elementTwo = char.Parse(Console.ReadLine());
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    if (matrix[rows, cols] == elementTwo)
+                    {
+                        bestRow = rows;
+                        bestCol = cols;
+
+                        Console.WriteLine($"({bestRow}, {bestCol})");
+                        return;
+                    }
+                    
+                }
+            }
+
+            Console.WriteLine($"{elementTwo} does not occur in the matrix");
+
+        }
+    }
+}
