74df4c17ebb77d085d2af4443a363ca599fdb5fd
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-11-25 19:10:01 +0000

    Add files via upload

---
+++ 04.MultiDimensionalArraysEx-e/02.2x2SquaresInMatrix/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._2x2SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int rows = input[0];
+            int cols = input[1];
+            char[,] matrix = new char[rows, cols];
+            FillMatrix(matrix);
+            //Console.WriteLine("-----------------");
+            //PrintMatrix(matrix);
+
+            int counter = 0;
+            for (int row = 0; row < matrix.GetLength(0)-1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1)-1; col++)
+                {
+                    if (AreEqual(matrix, row, col))
+                    {
+                        counter++;
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+
+        private static bool AreEqual(char[,] matrix, int row, int col)
+        {
+            return matrix[row, col] == matrix[row, col + 1] &&
+                   matrix[row, col] == matrix[row + 1, col] &&
+                   matrix[row, col] == matrix[row + 1, col];
+                    
+        }
+
+        private static void PrintMatrix(char[,]matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row,col]+" ");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+
+        private static void FillMatrix(char[,]matrix)
+        {
+            
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+        }
+    }
+}
