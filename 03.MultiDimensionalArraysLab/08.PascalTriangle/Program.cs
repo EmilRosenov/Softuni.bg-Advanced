865d218d0c8823cfc5203835cfa64cce06ae81c6
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-11-25 19:06:21 +0000

    Add files via upload

---
+++ 03.MultiDimensionalArraysLab/08.PascalTriangle/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _08.PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long[][] pascal = new long[n][];
+            int col = 1;
+            for (int row = 0; row < pascal.Length; row++)
+            {
+                pascal[row] = new long[col];
+                pascal[row][0] = 1;
+                pascal[row][pascal[row].Length - 1] = 1;
+
+
+                if (row>1)
+                {
+                    for (int cols = 1; cols < pascal[row].Length-1; cols++)
+                    {
+                        long[] prevRow = pascal[row - 1];
+                        long firstNum = prevRow[cols];
+                        long secondNum = prevRow[cols-1];
+                        pascal[row][cols] = firstNum + secondNum;
+                    }
+                }
+                col++;
+            }
+
+            for (int row = 0; row < pascal.Length; row++)
+            {
+                for (int column = 0; column < pascal[row].Length; column++)
+                {
+                    Console.Write(pascal[row][column] +" ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
