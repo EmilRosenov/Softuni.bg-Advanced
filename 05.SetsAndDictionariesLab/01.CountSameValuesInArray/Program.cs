f36bd1525e7024f425b8459a749b9fc805fac7e9
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-11-26 20:02:43 +0000

    Add files via upload

---
+++ 05.SetsAndDictionariesLab/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.SetsAndDictionariesLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] innput = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            var occurances = new Dictionary<double, int>();
+
+            foreach (var item in innput)
+            {
+                if (!occurances.ContainsKey(item))
+                {
+                    occurances.Add(item, new int());
+                }
+                occurances[item]++;
+            }
+
+            foreach (var (dKey, iValue) in occurances)
+            {
+                Console.WriteLine($"{dKey} - {iValue} times");
+            }
+        }
+    }
+}
