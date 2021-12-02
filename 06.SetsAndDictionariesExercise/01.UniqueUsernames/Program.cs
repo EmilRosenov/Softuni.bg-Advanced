04c82e5f1e0a2a99b10637049587cbc09a4c583d
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-02 12:12:31 +0000

    Add files via upload

---
+++ 06.SetsAndDictionariesExercise/01.UniqueUsernames/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.SetsAndDictionariesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var list = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string username = Console.ReadLine();
+                list.Add(username);
+            }
+            foreach (var item in list)
+            {
+                
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
