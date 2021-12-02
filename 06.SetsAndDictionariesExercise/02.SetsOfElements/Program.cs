d92bdc667306dc61498aa448cb617e21f410f44e
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-02 12:13:09 +0000

    Add files via upload

---
+++ 06.SetsAndDictionariesExercise/02.SetsOfElements/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lenghts = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = lenghts[0];
+            int m = lenghts[1];
+
+            var first = new HashSet<int>();
+            var second = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+                first.Add(numbers);
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+                second.Add(numbers);
+            }
+
+            var third = first.Intersect(second);
+            Console.WriteLine(string.Join(" ", third));
+        }
+    }
+}
