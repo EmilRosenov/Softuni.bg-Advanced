220d611826bcf39f37b22e06f273a71412bdfbe5
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-02 12:08:15 +0000

    Add files via upload

---
+++ 05.SetsAndDictionariesLab/05.RecordUniqueNames/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.RecordUniqueNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var namesList = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                namesList.Add(name);
+            }
+
+            foreach (var item in namesList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
