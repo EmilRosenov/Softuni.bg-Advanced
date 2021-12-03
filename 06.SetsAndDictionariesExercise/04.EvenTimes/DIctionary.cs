2fbf827bd22bf129cad9a4a064b2746d6aa8b6f4
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-03 14:17:25 +0000

    Add files via upload

---
+++ 06.SetsAndDictionariesExercise/04.EvenTimes/DIctionary.cs
@@ -0,0 +1,9 @@
+﻿namespace _04.EvenTimes
+{
+    internal class DIctionary<T1, T2>
+    {
+        public DIctionary()
+        {
+        }
+    }
+}
---
+++ 06.SetsAndDictionariesExercise/04.EvenTimes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var occurances = new Dictionary<string, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string number = Console.ReadLine();
+                if (!occurances.ContainsKey(number))
+                {
+                    occurances.Add(number, 0);
+                }
+                occurances[number]++;
+            }
+
+            foreach (var item in occurances)
+            {
+                if (item.Value%2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+        }
+    }
+}
