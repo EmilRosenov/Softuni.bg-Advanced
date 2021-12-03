f4d2df2f534006e4fea2611267d782deb98720f1
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-03 14:17:51 +0000

    Add files via upload

---
+++ 06.SetsAndDictionariesExercise/05.CountSymbols/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var occurances = new Dictionary<char, int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char current = input[i];
+                if (!occurances.ContainsKey(current))
+                {
+                    occurances.Add(current,0);
+                }
+                occurances[current]++;
+            }
+
+            foreach (var item in occurances.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
