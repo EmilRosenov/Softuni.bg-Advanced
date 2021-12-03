c68563fdaa56aeabfca7a6cf676978661a4c125c
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-03 14:21:40 +0000

    Add files via upload

---
+++ 07.StreamsFilesAndDirectoriesLab/02.StreamReader/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace _02.StreamReaderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (StreamReader reader = new StreamReader(@"C:\Users\Sony Vaio\Desktop\Softuni\Advanced\07.StreamsFilesAndDirectoriesLab\07.StreamsFilesAndDirectories\07.StreamsFilesAndDirectories\student.txt"))
+            {
+
+                string text = reader.ReadLine();
+
+                string fullText = reader.ReadToEnd();
+                Console.WriteLine(fullText);
+                
+
+               using (StreamWriter writer = new StreamWriter("../../../input.txt"))
+               {
+
+                    writer.Write(fullText);
+               
+               }
+            }
+            
+
+            
+        }
+    }
+}
