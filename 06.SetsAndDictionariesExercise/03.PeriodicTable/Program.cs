f92dd2c0186005a7c56cf538289f97997609df76
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-03 14:16:51 +0000

    Add files via upload

---
+++ 06.SetsAndDictionariesExercise/03.PeriodicTable/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var elements = new HashSet<string>();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    elements.Add(input[j]);
+                }
+            }
+
+            var sorted = elements.OrderBy(x => x);
+            foreach (var item in sorted)
+            {
+                Console.Write(string.Join(" ", item)+ " ");
+            }
+        }
+    }
+}
