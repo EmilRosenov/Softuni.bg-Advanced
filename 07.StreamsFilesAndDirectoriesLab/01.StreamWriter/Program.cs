ab2a04680a15d9762bfdb05219b012789bfa4e80
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-03 14:20:56 +0000

    Add files via upload

---
+++ 07.StreamsFilesAndDirectoriesLab/01.StreamWriter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace _07.StreamsFilesAndDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                using (StreamWriter writer = new StreamWriter($"../../../student0{i + 1}.txt"))
+                {
+
+                    writer.WriteLine("Hi there, I am a new student");
+                }
+                
+            }
+           
+        }
+    }
+}
