70e88800904d684152ddf82d122d3e32b15c7340
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-04 13:06:42 +0000

    Add files via upload

---
+++ 07.StreamsFilesAndDirectoriesLab/09.CalculateFolderSize/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace _09.CalculateFolderSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = @"C:\Users\Sony Vaio\Desktop\Softuni\Advanced\07.StreamsFilesAndDirectoriesLab\07.StreamsFilesAndDirectories\09.CalculateFolderSize\06. Folder Size\TestFolder\TestFolder2\";
+            string[] files = Directory.GetFiles(directoryPath);
+            double sum = 0;
+            for (int i = 0; i < files.Length; i++)
+            {
+                FileInfo info = new FileInfo(files[i]);
+                Console.WriteLine($"{ info.FullName} --> {info.Length} bytes");
+                sum += info.Length;
+            }
+
+            Console.WriteLine(sum/1024/1024);
+        }
+    }
+}
