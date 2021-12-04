888630da8cfab9b94217c24898d65a92c1e03d1d
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-04 13:02:06 +0000

    Add files via upload

---
+++ 07.StreamsFilesAndDirectoriesLab/04.LineNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace _04.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../Input.txt"))
+            {
+                
+                using (StreamWriter writer = new StreamWriter("../../../Output.txt"))
+                {
+                    string line = reader.ReadLine();
+                    int counter = 1;
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{counter}.  {line}");
+
+                        line = reader.ReadLine();
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
