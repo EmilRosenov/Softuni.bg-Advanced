425748c1d8f7628f53eb34bcfa162c34b6607945
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-04 13:05:58 +0000

    Add files via upload

---
+++ 07.StreamsFilesAndDirectoriesLab/08.DirectoryDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace _08.DirectoryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(Environment.NewLine,Directory.GetFiles("../../../")));
+            Console.WriteLine(Directory.Exists("../../../papkta"));
+            Directory.CreateDirectory("../../../papkata");
+            Console.WriteLine(Directory.Exists("../../../papkta"));
+            Directory.Delete("../../../papkata");
+        }
+    }
+}
