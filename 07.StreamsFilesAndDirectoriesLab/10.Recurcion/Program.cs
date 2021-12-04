efa903e5fe919920a90cc659de60d19fecf553fd
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-04 13:07:17 +0000

    Add files via upload

---
+++ 07.StreamsFilesAndDirectoriesLab/10.Recurcion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace _10.Recurtion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string directoryPath = Console.ReadLine();
+           double sum = GetDirectySize(directoryPath);
+          Console.WriteLine(sum);
+        }
+        static double GetDirectySize(string directoryPath)
+        {
+            string[] files = Directory.GetFiles(directoryPath);
+            double sum = 0;
+            string[] directories = Directory.GetDirectories(directoryPath);
+
+            for (int i = 0; i < directories.Length; i++)
+            {
+                sum += GetDirectySize(directories[i]);
+            }
+            for (int i = 0; i < files.Length; i++)
+            {
+                FileInfo info = new FileInfo(files[i]);
+                Console.WriteLine($"{info.FullName} --> {info.Length} bytes");
+                sum += info.Length;
+            }
+
+            return sum;
+        }
+    }
+}
