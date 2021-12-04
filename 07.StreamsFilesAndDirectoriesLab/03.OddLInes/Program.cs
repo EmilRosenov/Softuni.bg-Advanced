6b4b640f85694703d562b8937c64a9a086a901e2
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-04 13:01:32 +0000

    Add files via upload

---
+++ 07.StreamsFilesAndDirectoriesLab/03.OddLInes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace _03.OddLInes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../Input.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter("../../../Output.txt"))
+                { 
+                    int counter = 0;
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+
+                        if (counter % 2 != 0)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
