b15cf8d9714be03821e4802452ba5d9dcece6fef
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-05 20:54:17 +0000

    Add files via upload

---
+++ 08.StreamsFilesAndDirectoriesEx-e/01.EvenLines/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _01.EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            using (StreamReader reader = new StreamReader("../../../text.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    string symbolToReplace = "-,.!?";
+                    int counter = 0;
+                    string[] specialSymbols = new string[] { "-", ",", ".", "!", "?" };
+                    while (true)
+                    {
+                        string line = reader.ReadLine();
+                        if (line==null)
+                        {
+                            break;
+                        }
+
+                        foreach (var symbol in symbolToReplace)
+                        {
+                            line = line.Replace(symbol, '@');
+                        }
+                        if (counter % 2 == 0)
+                        {
+                            Console.WriteLine(string.Join(" ", line.Split().Reverse()));
+                            writer.WriteLine(line.Reverse());
+                        }
+
+                        counter++;
+                    }
+                }
+
+            }
+        }
+    }
+}
