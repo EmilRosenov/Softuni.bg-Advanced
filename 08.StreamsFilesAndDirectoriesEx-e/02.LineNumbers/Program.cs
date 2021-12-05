652d9ab82f5b01620a9e572b1444964bf96dd739
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-05 20:54:54 +0000

    Add files via upload

---
+++ 08.StreamsFilesAndDirectoriesEx-e/02.LineNumbers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("text.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    int counter = 1;
+                    while (true)
+                    {
+                        int lettersCount = 0;
+                        int punctuationsCount = 0;
+
+                        string line = reader.ReadLine();
+                        if (line == null)
+                        {
+                            break;
+                        }
+
+
+                        for (int i = 0; i < line.Length; i++)
+                        {
+                            char letter = line[i];
+                            if (char.IsLetter(letter))
+                            {
+                                lettersCount++;
+                            }
+                            if (char.IsPunctuation(letter))
+                            {
+                                punctuationsCount++;
+                            }
+                        }
+
+                        writer.WriteLine($"Line {counter}: {line} ({lettersCount})({punctuationsCount})");
+                        Console.WriteLine($"Line {counter}: {line} ({lettersCount})({punctuationsCount})");
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
