b721c8a86fbaf71f35eaeb8ae021917df68247d6
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-05 20:53:09 +0000

    Add files via upload

---
+++ 07.StreamsFilesAndDirectoriesLab/11.BasicVirus/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace _11.BasicVirus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string directoryPath =  Console.ReadLine();
+            double sum = GetDirectySize(directoryPath);
+            Console.WriteLine(sum);
+        }
+        static double GetDirectySize(string directoryPath)
+        {
+            string[] files = Directory.GetFiles(directoryPath);
+            double sum = 0;
+            string[] directories = Directory.GetDirectories(directoryPath);
+
+            for (int i = 0; i < directories.Length; i++)
+            {
+                sum += GetDirectySize(directories[i]);
+                //Directory.Delete(directories[i]); ** DO NOT RUN IT
+            }
+            for (int i = 0; i < files.Length; i++)
+            {
+                FileInfo info = new FileInfo(files[i]);
+                Console.WriteLine($"{info.FullName} --> {info.Length} bytes");
+                sum += info.Length;
+                //File.Delete(files[i]); ** DO NOT RUN IT
+            }
+
+            return sum;
+        }
+    }
+}
