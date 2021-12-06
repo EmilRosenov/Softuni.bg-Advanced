22a9797ee20f07c7778a1330ee6934f63ece2d28
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-06 18:04:43 +0000

    Add files via upload

---
+++ 08.StreamsFilesAndDirectoriesEx-e/04.CopyBinaryFile/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace _04.CopyBinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //File.Copy("copyMe.png", "copyMe-copy.png");
+            FileStream fileReader = new FileStream("copyMe.png", FileMode.Open);
+            FileStream fileWriter = new FileStream("../../../copyMe-copy.png", FileMode.Create);
+            byte[] arrayOfBytes = new byte[256];
+
+
+            while (true)
+            {
+                int currentBytes = fileReader.Read(arrayOfBytes, 0, arrayOfBytes.Length);
+                if (currentBytes == 0)
+                {
+                    fileWriter.Flush();
+                    break;
+                }
+                fileWriter.Write(arrayOfBytes, 0, arrayOfBytes.Length);
+                
+            }
+            Console.WriteLine("Done!");
+        }
+    }
+}
