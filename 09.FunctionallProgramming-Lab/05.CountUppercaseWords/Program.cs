2d5a272f8f01a7d84fccad12c24f675a52b83911
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-07 23:05:19 +0000

    Add files via upload

---
+++ 09.FunctionallProgramming-Lab/05.CountUppercaseWords/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            List<string> uppercaseWords = new List<string>();
+            foreach (string word in text)
+            {
+                if (word.StartsWith(char.ToUpper(word[0])))
+                {
+                    uppercaseWords.Add(word);
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine,uppercaseWords));
+        }
+    }
+}
