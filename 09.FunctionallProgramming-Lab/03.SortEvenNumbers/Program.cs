b7b3d411a770ac8a493b882f98a3e94d6149fc52
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-07 22:49:52 +0000

    Add files via upload

---
+++ 09.FunctionallProgramming-Lab/03.SortEvenNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] line = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            List<int> evenList = new List<int>();
+            foreach (int element in line)
+            {
+                if (element % 2 ==0)
+                {
+                    evenList.Add(element);
+                }
+            }
+            Console.WriteLine(string.Join(", ",evenList.OrderBy(x=>x)));
+        }
+    }
+}
