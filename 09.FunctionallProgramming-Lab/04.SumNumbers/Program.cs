b820c016bb1c583befa11be1324e510a91e6222c
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-07 22:55:13 +0000

    Add files via upload

---
+++ 09.FunctionallProgramming-Lab/04.SumNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] line = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            
+            int sum = line.Sum();
+            int count = line.Length;
+            Console.WriteLine(count);
+            Console.WriteLine(sum);
+
+        }
+    }
+}
