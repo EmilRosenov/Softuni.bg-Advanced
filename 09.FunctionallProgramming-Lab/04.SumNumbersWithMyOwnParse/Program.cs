84aa86fec207c797384ea284cb4d45950c9ec1dd
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-08 14:18:22 +0000

    Add files via upload

---
+++ 09.FunctionallProgramming-Lab/04.SumNumbersWithMyOwnParse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.SumNumbersWithMyOwnParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] line = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(MyParse)
+                .ToArray();
+            
+            Console.WriteLine(line.Length);
+            Console.WriteLine(line.Sum());
+        }
+        static int MyParse(string numberAsString)
+        {
+            int number = 0;
+            foreach (var digit in numberAsString)
+            {
+                number *= 10;
+                number += (digit - '0');
+            }
+
+            return number;
+        }
+    }
+}
