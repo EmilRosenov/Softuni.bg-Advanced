3e21f9cc587ea783254eb98996ad1f820989abe5
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-08 18:38:40 +0000

    Add files via upload

---
+++ 10.FunctionalProgrammingExercise/01.ActionPoint/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Action<string[]> printResult = input 
+                =>  Console.WriteLine(string.Join(Environment.NewLine,input));
+            printResult(input);
+        }
+    }
+}
