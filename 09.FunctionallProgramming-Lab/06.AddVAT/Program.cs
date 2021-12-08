b6860ace4a165bf70637d0e7c26282a154f6440a
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-08 14:59:48 +0000

    Add files via upload

---
+++ 09.FunctionallProgramming-Lab/06.AddVAT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(", ").Select(double.Parse).ToArray();
+            var numbersWithVAT = numbers.Select(number => number * 1.2);
+            foreach (var number in numbersWithVAT)
+            {
+                Console.WriteLine($"{number:F2}");
+            }
+        }
+    }
+}
