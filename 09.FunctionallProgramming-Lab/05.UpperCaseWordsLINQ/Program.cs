6d9d92c2a977660546f860c8b0f009147569678f
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-08 14:48:54 +0000

    Add files via upload

---
+++ 09.FunctionallProgramming-Lab/05.UpperCaseWordsLINQ/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.UpperCaseWordsLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            var words = text.Where(word => char.IsUpper(word[0]));
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
