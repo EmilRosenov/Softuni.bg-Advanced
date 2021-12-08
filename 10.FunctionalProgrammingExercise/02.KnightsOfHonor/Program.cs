1d51f682a3e168e5d14a8476ea31d468ab2485f2
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-08 19:31:52 +0000

    Add files via upload

---
+++ 10.FunctionalProgrammingExercise/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            List<string> namesList = new List<string>();
+            foreach (var name in names)
+            {
+                namesList.Add(name);
+            }
+
+            Action<List<string>> appendSir =
+                namesList => namesList.ForEach(x => Console.WriteLine("Sir " + (x)));
+            appendSir(namesList);
+        }
+    }
+}
