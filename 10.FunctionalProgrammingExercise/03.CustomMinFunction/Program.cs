9ab02fdb073ec06020744d441d0c00716fd92e9a
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-09 14:53:00 +0000

    Add files via upload

---
+++ 10.FunctionalProgrammingExercise/03.CustomMinFunction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] line = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+
+            Func<int[], int> smallestInteger = line=>
+            {
+                int minValue = int.MaxValue;
+
+                foreach (var item in line)
+                {
+                    if (item<minValue)
+                    {
+                        minValue = item;
+                    }
+                }
+                return minValue;
+            };
+            Console.WriteLine(smallestInteger(line));
+        }
+    }
+}
