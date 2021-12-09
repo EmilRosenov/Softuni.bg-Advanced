7c4cd151aa3a53ede5bed97e1665e940ee083fd1
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-09 19:52:47 +0000

    Add files via upload

---
+++ 10.FunctionalProgrammingExercise/04.FindEvenOrOddWithWhere/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.FindEvenOrOddWithWhere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+
+            string inputEvenOrOdd = Console.ReadLine();
+            Predicate<int> isEven = number => number % 2 == 0;
+            List<int> list = new List<int>();
+            Action<int[]> printNumbers = list
+                => Console.WriteLine(string.Join(" ", list));
+
+            for (int i = numbers[0]; i <= numbers[1]; i++)
+            {
+                list.Add(i);
+            }
+
+            if (inputEvenOrOdd == "even")
+            {
+                printNumbers(list.Where(x => isEven(x)).ToArray());
+            }
+            else
+            {
+                printNumbers(list.Where(x => !isEven(x)).ToArray());
+            }
+
+        }
+    }
+}
