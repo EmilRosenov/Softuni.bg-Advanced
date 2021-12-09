e331f0e6f17a89046d6844a8ec91d0996d1d5daf
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-09 22:59:20 +0000

    Add files via upload

---
+++ 10.FunctionalProgrammingExercise/07.PredicateOfNames/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.PredicateOfNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int namesMaxLenght = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Action<List<string>> printList = collection =>
+            Console.WriteLine(string.Join(Environment.NewLine,collection));
+            printList(names.Where(x => x.Length <= namesMaxLenght).ToList());
+        }
+    }
+}
