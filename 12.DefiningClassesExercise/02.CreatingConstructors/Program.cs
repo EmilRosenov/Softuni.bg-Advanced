bed5aecee117b28c287a7d1c45c1c819ea92a903
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-21 22:41:59 +0000

    Add files via upload

---
+++ 12.DefiningClassesExercise/02.CreatingConstructors/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person personOne = new Person(12);
+            Console.WriteLine(personOne.Name);
+            Console.WriteLine(personOne.Age);
+        }
+    }
+}
