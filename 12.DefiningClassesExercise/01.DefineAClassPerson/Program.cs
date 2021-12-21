7a8deae6c386d5d218ec3d35c3f19aaffe5d5c51
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-21 22:28:08 +0000

    Add files via upload

---
+++ 12.DefiningClassesExercise/01.DefineAClassPerson/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person personOne = new Person("Peter", 20);
+            Person personTwo = new Person();
+            personTwo.Name = "George";
+            personTwo.Age = 18;
+            Person personThree = new Person("Jose", 43);
+
+            Console.WriteLine($"{ personOne.Name} {personOne.Age}");
+            Console.WriteLine($"{ personTwo.Name} {personTwo.Age}");
+            Console.WriteLine($"{ personThree.Name} {personThree.Age}");
+        }
+    }
+}
