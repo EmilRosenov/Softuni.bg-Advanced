fa6dc0f0489e41560fab56e86bac62e01e9f1339
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-22 21:14:14 +0000

    Add files via upload

---
+++ 12.DefiningClassesExercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.DateModifier
+{
+    public class DateModifier
+    {
+        public string startAsString;
+        public string endAsString;
+
+        public int CalculateDifference(string startAsString, string endAsString)
+        {
+            DateTime startDate = DateTime.Parse(startAsString);
+            DateTime endDate = DateTime.Parse(endAsString);
+            int result = (int)Math.Abs((startDate - endDate).TotalDays);
+
+            return result;
+        }
+    }
+}
---
+++ 12.DefiningClassesExercise/05.DateModifier/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _05.DateModifier
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string startDate = Console.ReadLine();
+            string endDate = Console.ReadLine();
+
+            DateModifier dateModifier = new DateModifier();
+
+            int output = dateModifier.CalculateDifference(startDate, endDate);
+            Console.WriteLine(output);
+        }
+    }
+}
