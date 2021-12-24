8b093054d91c08a15eca2634b662b74b50ee351d
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-24 20:00:49 +0000

    Add files via upload

---
+++ 12.DefiningClassesExercise/08.CarSalesman/Car.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08.CarSalesman
+{
+    public class Car
+    {
+
+        public string Model { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public int Weight { get; set; }
+
+        public string Color { get; set; }
+
+        public Car(string model, Engine engine)
+        {
+            this.Model = model;
+            this.Engine = engine;
+        }
+
+        public Car(string model, Engine engine, int weight)
+            : this(model, engine)
+        {
+           Weight = weight;
+        }
+
+        public Car(string model, Engine engine, string color)
+            : this(model, engine)
+        {
+           Color = color;
+        }
+
+        public Car(string model, Engine engine, int weight, string color)
+            : this(model, engine, weight)
+        {
+            Color = color;
+        }
+
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+
+            result.AppendLine($"{Model}:");
+            result.AppendLine($"{Engine}");
+
+            if (Weight == 0)
+            {
+                result.AppendLine($"  Weight: n/a");
+            }
+            else
+            {
+                result.AppendLine($"  Weight: {Weight}");
+            }
+
+            if (Color == null)
+            {
+                result.Append($"  Color: n/a");
+            }
+            else
+            {
+                result.Append($"  Color: {Color}");
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ 12.DefiningClassesExercise/08.CarSalesman/Engine.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08.CarSalesman
+{
+    public class Engine
+    {
+        public string Model { get; set; }
+
+        public int Power { get; set; }
+
+        public int Displacement { get; set; }
+
+        public string Efficiency { get; set; }
+
+        public Engine(string model, int power)
+        {
+            Model = model;
+            Power = power;
+        }
+
+        public Engine(string model, int power, int displacement)
+            : this(model, power)
+        {
+            Displacement = displacement;
+        }
+
+        public Engine(string model, int power, string efficiency)
+            : this(model, power)
+        {
+            Efficiency = efficiency;
+        }
+
+        public Engine(string model, int power, int displacement, string efficiency)
+            : this(model, power, displacement)
+        {
+            Efficiency = efficiency;
+        }
+
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+
+            result.AppendLine($"  {Model}:");
+            result.AppendLine($"    Power: {Power}");
+
+            if (Displacement == 0)
+            {
+                result.AppendLine($"    Displacement: n/a");
+            }
+            else
+            {
+                result.AppendLine($"    Displacement: {Displacement}");
+            }
+
+            if (Efficiency == null)
+            {
+                result.Append($"    Efficiency: n/a");
+            }
+            else
+            {
+                result.Append($"    Efficiency: {Efficiency}");
+            }
+
+            return result.ToString();
+        }
+    }
+}
