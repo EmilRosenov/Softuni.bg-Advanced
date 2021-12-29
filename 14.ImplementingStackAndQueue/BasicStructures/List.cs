bd096dda85e9de4e5fcee040e8c085dd79355f14
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-29 23:21:25 +0000

    Add files via upload

---
+++ 14.ImplementingStackAndQueue/BasicStructures/List.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicStructures
+{
+    public class List
+    {
+        private const int DefaultCapacity = 2;
+        private int[] items;
+
+        public List()
+        {
+            items = new int[DefaultCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public int this[int i]
+        {
+            get
+            {
+                IsInRange(i);
+
+                return items[i];
+            }
+            set
+            {
+                IsInRange(i);
+
+                items[i] = value;
+            }
+        }
+
+       
+
+        public void Add(int element)
+        {
+            if (Count==items.Length)
+            {
+                Resize();
+            }
+            items[Count++] = element;
+
+        }
+
+        
+        public int RemoveAt(int index)
+        {
+            IsInRange(index);
+
+            int element = items[index];
+            items[index] = 0;
+
+            for (int i = index; i < Count; i++)
+            {
+                items[i] = items[i + 1];
+            }
+            Count--;
+
+            //10-2->[] []
+            if (Count <= items.Length / 4)
+            {
+                Shrink();
+            }
+            return element;
+        }
+
+        private void Shrink()
+        {
+            int[] tempArray = new int[items.Length / 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                tempArray[i] = items[i];
+            }
+
+            items = tempArray;
+        }
+
+        public bool Contains(int element)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i]==element)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            if (firstIndex < 0 ||firstIndex >= Count 
+                || secondIndex<0 || secondIndex >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            int tempElement = items[firstIndex];
+            items[firstIndex] = items[secondIndex];
+            items[secondIndex] = tempElement;
+        }
+        public void Print()
+        {
+            Console.WriteLine(string.Join(" ", items)); 
+        }
+        private void IsInRange(int i)
+        {
+            if (i < 0 || i >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+        private void Resize()
+        {
+            int[] tempArray = new int[items.Length * 2];
+            for (int i = 0; i < items.Length; i++)
+            {
+                tempArray[i] = items[i];
+            }
+            items = tempArray;
+        }
+
+    }
+}
---
+++ 14.ImplementingStackAndQueue/BasicStructures/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BasicStructures
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List list = new List();
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            list.Add(30);
+            list.Add(30);
+            list.Print();
+            list.Swap(0, 1);
+            list.Print();
+            list.RemoveAt(0);
+            list.RemoveAt(0);
+            list.Print();
+            //list.RemoveAt(0);
+            //list.RemoveAt(0);
+            Console.WriteLine(list.Count);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            //list.Add(30);
+
+
+            
+        }
+        // void Add(int element) - Adds the given element to the end of the list
+        // int RemoveAt(int index) - Removes the element at the given index
+        // bool Contains(int element) - Checks if the list contains the given element returns(True or False)
+        // void Swap(int firstIndex, int secondIndex) - Swaps the elements at the given indexes
+           
+    }
+}
