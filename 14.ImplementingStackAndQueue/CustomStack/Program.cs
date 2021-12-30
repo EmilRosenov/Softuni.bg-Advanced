dc405c5d52758070e3acbf99b131a85233fe9f77
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-12-30 20:23:25 +0000

    Add files via upload

---
+++ 14.ImplementingStackAndQueue/CustomStack/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Console.WriteLine(stack.Peek());
+            stack.Push(4);
+            stack.Push(5);
+            stack.Pop();
+            stack.ForEach(Console.WriteLine);
+        }
+    }
+}
---
+++ 14.ImplementingStackAndQueue/CustomStack/Stack.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class Stack
+    {
+        private const int Inititalcapacity = 4;
+        private int[] elements;
+        public Stack()
+        {
+            elements = new int[Inititalcapacity];
+        }
+        public int Count { get; private set; }
+        public void Push(int element)
+        {
+            if (Count==elements.Length)
+            {
+                Resize();
+            }
+            elements[Count++] = element;
+        }
+        public int Pop()
+        {
+            if (Count==0)
+            {
+                throw new InvalidOperationException();
+            }
+            int element = elements[Count - 1];
+
+            Count--;
+            if (Count<=elements.Length)
+            {
+                Shrink();
+            }
+
+            return element;
+        }
+
+       
+        public int Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return elements[Count - 1];
+        }
+        public void ForEach(Action<int> action)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                action(elements[i]);
+            }
+        }
+
+
+        private void Resize()
+        {
+            int[] copyArray = new int[elements.Length * 2];
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                copyArray[i] = elements[i];
+            }
+            elements = copyArray;
+            //the code above equals this row:
+            //Array.Copy(elements, copyArray, elements.Length);
+            
+        }
+        private void Shrink()
+        {
+            int[] copyArray = new int[elements.Length / 2];
+            for (int i = 0; i < Count; i++)
+            {
+                copyArray[i] = elements[i];
+            }
+            elements = copyArray;
+        }
+
+
+
+    }
+}
