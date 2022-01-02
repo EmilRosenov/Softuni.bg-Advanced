167a241e6428f402a46fa480ab6b579f915a6bed
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-02 16:40:48 +0000

    Add files via upload

---
+++ 15.Generics/01.BoxOfT/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> internalList = new List<T>();
+        public void Add(T element)
+        {
+            internalList.Add(element);
+
+        }
+        public int Count 
+        {
+            get
+            {
+                return internalList.Count;
+            }
+        }
+        public T Remove()
+        {
+            T element = internalList[internalList.Count - 1];
+            internalList.RemoveAt(internalList.Count - 1);
+            return element;
+
+        }
+    }
+}
---
+++ 15.Generics/01.BoxOfT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var box = new Box<string>();
+            box.Add("Emil");
+            box.Add("Pesho");
+            box.Add("Gosho");
+            box.Add("Niki");
+            Console.WriteLine(box.Remove());
+            Console.WriteLine(box.Remove());
+            Console.WriteLine(box.Count);
+        }
+    }
+}
