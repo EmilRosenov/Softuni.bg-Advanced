3eeb6ddb5aafc43945bb146b104e612010787a27
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-03 21:58:30 +0000

    Add files via upload

---
+++ 15.Generics/02.GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,15 @@
+﻿namespace GenericArrayCreator
+{
+    public static class ArrayCreator
+    {
+        public static T[] Create<T>(int lenght, T item)
+        {
+            T[] array = new T[lenght];
+            for (int i = 0; i < lenght; i++)
+            {
+                array[i] = item;
+            }
+            return array;
+        }
+    }
+}
