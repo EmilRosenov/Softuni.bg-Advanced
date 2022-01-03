005a5757e5dc54abe70e5a4663c89031299f04da
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-03 21:59:07 +0000

    Add files via upload

---
+++ 15.Generics/03.GenericScale/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var a = new string[] { "left" };
+            var b = new string[] { "left" };
+
+            Console.WriteLine(a==b);
+           
+        }
+    }
+}
