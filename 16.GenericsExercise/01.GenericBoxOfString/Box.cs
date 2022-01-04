07404e6bd5e45604aa4940d1e67b450df762c5bc
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-04 22:31:50 +0000

    Add files via upload

---
+++ 16.GenericsExercise/01.GenericBoxOfString/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+            
+
+            return sb.ToString().TrimEnd(); 
+        }
+    }
+}
---
+++ 16.GenericsExercise/01.GenericBoxOfString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.GenericBoxOfString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                box.Add(input);
+            }
+
+            Console.WriteLine(box);
+        }
+    }
+}
