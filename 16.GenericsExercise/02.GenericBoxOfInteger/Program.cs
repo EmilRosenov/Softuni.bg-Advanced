f443427ae15140a2d8c9dddb70292a1659bd631f
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-04 22:32:19 +0000

    Add files via upload

---
+++ 16.GenericsExercise/02.GenericBoxOfInteger/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02.GenericBoxOfInteger
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Box<int> box = new Box<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                box.Add(input);
+            }
+
+            Console.WriteLine(box);
+        }
+    }
+}
