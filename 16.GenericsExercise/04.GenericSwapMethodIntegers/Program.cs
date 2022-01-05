b3b54fbc37c6cbf4422f8c8275f25a245939f755
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-05 15:22:44 +0000

    Add files via upload

---
+++ 16.GenericsExercise/04.GenericSwapMethodIntegers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.GenericSwapMethodIntegers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Box<int> box = new Box<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                box.Add(input);
+            }
+
+            int[] inputSwap = Console.ReadLine()
+                .Split(" ").Select(int.Parse).ToArray();
+            int first = inputSwap[0];
+            int second = inputSwap[1];
+
+            box.Swap(first, second);
+            Console.WriteLine(box);
+        }
+    }
+}
