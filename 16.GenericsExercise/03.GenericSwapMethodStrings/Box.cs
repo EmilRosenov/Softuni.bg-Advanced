224edafeaec418d38577dcf97b9693b53ac46c24
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-05 15:22:12 +0000

    Add files via upload

---
+++ 16.GenericsExercise/03.GenericSwapMethodStrings/Box.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.GenericSwapMethodStrings
+{
+    class Box<T>
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+        public void Swap(int first, int second)
+        {
+            T temp = items[first];
+            items[first] = items[second];
+            items[second] = temp;
+            
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in items)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+
+            return sb.ToString().TrimEnd(); 
+        }
+
+    }
+}
---
+++ 16.GenericsExercise/03.GenericSwapMethodStrings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.GenericSwapMethodStrings
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                box.Add(input);
+            }
+
+            int[] inputSwap = Console.ReadLine()
+                .Split(" ").Select(int.Parse).ToArray();
+            int first = inputSwap[0];
+            int second = inputSwap[1];
+
+            box.Swap(first,second);
+            Console.WriteLine(box);
+        }
+    }
+}
