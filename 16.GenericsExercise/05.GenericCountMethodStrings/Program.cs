1022817c150ec0233ae9f5fa20f698b1557e3399
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-06 20:33:14 +0000

    Add files via upload

---
+++ 16.GenericsExercise/05.GenericCountMethodStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05.GenericCountMethodStrings
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                box.Add(input);
+            }
+            string comparer = Console.ReadLine();
+            Console.WriteLine(box.GetGenericCount(comparer));
+            
+        }
+    }
+}
