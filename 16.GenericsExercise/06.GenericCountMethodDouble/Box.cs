8ec90304861b9b2d3f7931b058bbcf533388911b
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-06 20:33:52 +0000

    Add files via upload

---
+++ 16.GenericsExercise/06.GenericCountMethodDouble/Box.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.GenericCountMethodDouble
+{
+    public class Box<T> where T : IComparable
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+        public void Swap(int first, int second)
+        {
+            T temp = items[first];
+            items[first] = items[second];
+            items[second] = temp;
+
+        }
+        public int GetGenericCount(T elementToCompare)
+        {
+            int counter = 0;
+            foreach (var item in items)
+            {
+                if (item.CompareTo(elementToCompare) > 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in items)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 16.GenericsExercise/06.GenericCountMethodDouble/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06.GenericCountMethodDouble
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            Box<double> box = new Box<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+                box.Add(input);
+            }
+            double comparer = double.Parse(Console.ReadLine());
+            Console.WriteLine(box.GetGenericCount(comparer));
+
+        }
+    }
+}
