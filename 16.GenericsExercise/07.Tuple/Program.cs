6dc96176a5faaa0cd4daeea277f6c411a3920712
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-06 20:34:37 +0000

    Add files via upload

---
+++ 16.GenericsExercise/07.Tuple/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _07.Tuple
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] personDetails = Console.ReadLine().Split();
+            string[] personBeersAmount = Console.ReadLine().Split();
+            string[] numbers = Console.ReadLine().Split();
+
+            string name = $"{personDetails[0]} {personDetails[1]}";
+            string personAddress = personDetails[2];
+            MyTuple<string, string> person =
+                new MyTuple<string,string>(name,personAddress);
+
+            string drinkersName = personBeersAmount[0];
+            int litersBeer = int.Parse(personBeersAmount[1]);
+            MyTuple<string, int> nameBeer =
+                new MyTuple<string, int>(drinkersName, litersBeer);
+
+            int number = int.Parse(numbers[0]);
+            double doubleNumber = double.Parse(numbers[1]);
+            MyTuple<int, double> numbersTuple =
+                new MyTuple<int, double>(number, doubleNumber);
+
+
+            Console.WriteLine(person.GetItems());
+            Console.WriteLine(nameBeer.GetItems());
+            Console.WriteLine(numbersTuple.GetItems());
+        }
+    }
+}
---
+++ 16.GenericsExercise/07.Tuple/Tuple.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.Tuple
+{
+    public class MyTuple< T1, T2>
+    {
+        public MyTuple(T1 itemOne, T2 itemTwo)
+        {
+            ItemOne = itemOne;
+            ItemTwo = itemTwo;
+        }
+        public T1 ItemOne { get; set; }
+        public T2 ItemTwo { get; set; }
+
+        public string GetItems()
+        {
+            return $"{ItemOne} -> {ItemTwo}";
+        }
+    }
+}
