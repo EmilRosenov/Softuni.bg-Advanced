4805c268a93d6f7e8cb97778fdb9505b61f207a3
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-07 22:09:46 +0000

    Add files via upload

---
+++ 16.GenericsExercise/08.ThreeUple/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace _08.ThreeUple
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] firstInput = Console.ReadLine().Split();
+            string[] secondInput = Console.ReadLine().Split();
+            string[] thirdInput = Console.ReadLine().Split();
+
+            string name = $"{firstInput[0]} {firstInput[1]}";
+            string street = firstInput[2];
+            StringBuilder sb = new StringBuilder();
+            for (int i = 3; i < firstInput.Length; i++)
+            {
+                sb.Append(firstInput[i] + " ");
+            }
+            string city = sb.ToString().TrimEnd();
+
+            ThreeUple<string, string, string> nameStreetCity =
+                new ThreeUple<string, string, string>(name, street, city);
+            Console.WriteLine(nameStreetCity.GetItems());
+
+            string beerDrinkersName = secondInput[0];
+            int drinksNeeded = int.Parse(secondInput[1]);
+            bool drunkOrNot = secondInput[2] =="drunk";
+            ThreeUple<string,int, bool> beerNameAmountState = 
+                new ThreeUple<string, int, bool>
+                (beerDrinkersName, drinksNeeded, drunkOrNot);
+            Console.WriteLine(beerNameAmountState.GetItems());
+
+
+            string nameTwo = thirdInput[0];
+            double doubleNumber = double.Parse(thirdInput[1]);
+            string workplace = thirdInput[2];
+            ThreeUple<string, double, string> nameAmountWorkPlace =
+                new ThreeUple<string, double, string>
+                (nameTwo, doubleNumber, workplace);
+            Console.WriteLine(nameAmountWorkPlace.GetItems());
+
+
+        }
+    }
+}
---
+++ 16.GenericsExercise/08.ThreeUple/ThreeUple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08.ThreeUple
+{
+    public class ThreeUple<Type1,Type2,Type3>
+    {
+        public ThreeUple(Type1 first,Type2 second,Type3 third)
+        {
+            First = first;
+            Second = second;
+            Third = third;
+        }
+        public Type1 First { get; set; }
+        public Type2 Second { get; set; }
+        public Type3 Third { get; set; }
+
+        
+        public string GetItems()
+        {
+            return $"{First} -> {Second} -> {Third}";
+        }
+    }
+}
