7a4594b76453d01868f5abe5dbe8995ffd0a2b92
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-10 13:33:34 +0000

    Add files via upload

---
+++ 17.IteratorsAndComparators/04.BookComparator/BookComparator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class BookComparator : IComparer<Book>
+    {
+        public int Compare(Book x, Book y)
+        {
+            return y.Year.CompareTo(x.Year);
+        }
+    }
+}
