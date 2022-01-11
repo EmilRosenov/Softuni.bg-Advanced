887087a4b62e2c103dea55e4e1a23bdf503c5000
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-11 22:29:59 +0000

    Add files via upload

---
+++ 18.IteratorsAndComparatorsExercise/03.Stack/CustomStack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03.Stack
+{
+    public class CustomStack<T>: IEnumerable<T>
+    {
+        private List<T> customStack;
+        public int Count { get; set; }
+
+        public CustomStack(params T[] data)
+        {
+            customStack = new List<T>(data);
+            
+        }
+
+        public void Push(params T[] num)
+        {
+            customStack.AddRange(num);
+
+            foreach (var element in num)
+            {
+                Count++;
+            }
+            
+        }
+        public void Pop()
+        {
+            if (customStack.Count==0)
+            {
+                Console.WriteLine("No elements");
+                   
+            }
+            else
+            {
+                customStack.RemoveAt(customStack.Count - 1);
+                Count--;
+            }
+            
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in customStack.ToArray().Reverse())
+            {
+                yield return item;
+
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+       
+    }
+}
---
+++ 18.IteratorsAndComparatorsExercise/03.Stack/StartUp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Stack
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                     .Split(new char[] {' ', ','},StringSplitOptions.RemoveEmptyEntries);
+            int[] numbersArray = new int[input.Length - 1];
+
+            for (int i = 0; i < input.Length-1; i++)
+            {
+                numbersArray[i] = int.Parse(input[i+1]);
+            }
+
+            CustomStack<int> customStack = new CustomStack<int>();
+
+            while (input[0]!="END")
+            {
+                
+                if (input[0]=="Push")
+                {
+                   
+                    customStack.Push(numbersArray);
+                }
+                else if (input[0] =="Pop")
+                {
+                    customStack.Pop();
+                }
+
+                input = Console.ReadLine().Split();
+
+                numbersArray = new int[input.Length - 1];
+
+                for (int i = 0; i < input.Length - 1; i++)
+                {
+                    numbersArray[i] = int.Parse(input[i + 1]);
+                }
+
+            }
+
+            foreach (var item in customStack.ToList())
+            {
+                if (customStack.Count==0)
+                {
+                    throw new ArgumentException("No elements");
+                }
+
+                Console.WriteLine(item);
+            }
+            foreach (var item in customStack.ToList())
+            {
+                if (customStack.Count == 0)
+                {
+                    throw new ArgumentException("No elements");
+                }
+
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
