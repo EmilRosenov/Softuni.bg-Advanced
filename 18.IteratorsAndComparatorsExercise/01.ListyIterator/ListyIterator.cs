35a40a455b2116967f2566d36e79658e16a13934
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-11 17:24:37 +0000

    Add files via upload

---
+++ 18.IteratorsAndComparatorsExercise/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace _01.ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private int currIndex;
+        private List<T> collection;
+
+       
+        public ListyIterator(params T[] data)
+        {
+            collection = new List<T>(data);
+            currIndex = 0;
+        }
+
+        public void Print()
+        {
+            if (collection.Count<=0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+                
+            }
+            else
+            {
+                Console.WriteLine(collection[currIndex]);
+            }
+
+        }
+        public bool HasNext() => currIndex < collection.Count - 1;
+        
+        public bool Move()
+        {
+            bool canMove = HasNext(); 
+            if (canMove)
+            {
+                currIndex++;     
+            }
+            return canMove;
+        }
+
+        public void Reset() { }
+
+        public void Dispose() { }
+
+    }
+}
---
+++ 18.IteratorsAndComparatorsExercise/01.ListyIterator/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ListyIterator
+{
+    public class SrartUp
+    {
+        public static void Main(string[] args)
+        {
+            string command = "";
+            ListyIterator<string> listy = null;
+           
+
+            while ((command = Console.ReadLine())!="END")
+            {
+                var token = command.Split();
+                if (token[0]=="Create")
+                {
+                    listy = new ListyIterator<string>(token.Skip(1).ToArray());
+                }
+                else if (token[0] =="HasNext")
+                {
+                    Console.WriteLine(listy.HasNext());
+                }
+                else if (token[0] == "Move")
+                {
+                    Console.WriteLine(listy.Move());
+                }
+                else if (token[0] == "Print")
+                {
+                    listy.Print();
+                }
+            }
+
+            //string[] array = new string[command.Length - 1];
+            //for (int i = 0; i < command.Length - 1; i++)
+            //{
+            //    array[i] = command[i + 1];
+            //}
+
+            //ListyIterator<string> listy = new ListyIterator<string>(array);
+        }
+    }
+}
