87775c0dafda77f31e6ac471e6b9533ca8456a09
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-11 17:52:50 +0000

    Add files via upload

---
+++ 18.IteratorsAndComparatorsExercise/02.Colllection/ListyIterator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace _02.Colllection
+{
+    public class ListyIterator<T> : IEnumerable<T>, IEnumerator<T>
+    {
+        private int currIndex;
+        private List<T> collection;
+
+        public T Current => throw new NotImplementedException();
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public ListyIterator(params T[] data)
+        {
+            collection = new List<T>(data);
+            currIndex = 0;
+        }
+
+        public void Print()
+        {
+            if (collection.Count <= 0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+
+            }
+            else
+            {
+                Console.WriteLine(collection[currIndex]);
+            }
+
+        }
+        public bool HasNext() => currIndex < collection.Count - 1;
+
+        public bool Move()
+        {
+            bool canMove = HasNext();
+            if (canMove)
+            {
+                currIndex++;
+            }
+            return canMove;
+        }
+
+        public void PrintAll()
+        {
+            foreach (var item in collection)
+            {
+                Console.Write(item +" ");
+            }
+            Console.WriteLine();
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in collection)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public bool MoveNext()
+        {
+            if (++currIndex < collection.Count)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset() { }
+
+
+        public void Dispose() { }
+
+    }
+}
---
+++ 18.IteratorsAndComparatorsExercise/02.Colllection/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Colllection
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string command = "";
+            ListyIterator<string> listy = null;
+
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                var token = command.Split();
+                if (token[0] == "Create")
+                {
+                    listy = new ListyIterator<string>(token.Skip(1).ToArray());
+                }
+                else if (token[0] == "HasNext")
+                {
+                    Console.WriteLine(listy.HasNext());
+                }
+                else if (token[0] == "Move")
+                {
+                    Console.WriteLine(listy.Move());
+                }
+                else if (token[0] == "Print")
+                {
+                    listy.Print();
+                }
+                else if (token[0] == "PrintAll")
+                {
+                    listy.PrintAll();
+                }
+            }
+        }
+    }
+}
