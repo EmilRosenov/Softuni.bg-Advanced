68bbc28716fdd1d2fa46e4ccba92c7b35429608e
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-12 17:06:55 +0000

    Add files via upload

---
+++ 18.IteratorsAndComparatorsExercise/04.Froggy/Lake.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04.Froggy
+{
+    public class Lake: IEnumerable<int>
+    {
+        private int[] stonePath;
+        public Lake(int[] data)
+        {
+            stonePath = data; 
+        }
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < stonePath.Length; i++)
+            {
+                if (i % 2==0)
+                {
+                    yield return stonePath[i];
+                }
+                
+            }
+
+            for (int i = stonePath.Length - 1; i >= 0; i--)
+            {
+                if (i % 2 !=0)
+                {
+                    yield return stonePath[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        
+    }
+}
---
+++ 18.IteratorsAndComparatorsExercise/04.Froggy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Froggy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Lake path = new Lake(input);
+            Console.WriteLine(string.Join(", ",path));
+        }
+    }
+}
