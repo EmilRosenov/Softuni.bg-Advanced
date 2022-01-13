d6019620a4c37b83bfa36dd79967bf5d3a4d7eee
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-13 05:08:38 +0000

    Add files via upload

---
+++ 18.IteratorsAndComparatorsExercise/05.ComparingObjects/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _05.ComparingObjects
+{
+    public class Person:IComparable<Person>
+    {
+        private string name;
+        private int age;
+        private string city;
+        private int count;
+        List<Person> personList = new List<Person>();
+       
+        
+        public Person(string name, int age, string city)
+        {
+            Name = name;
+            Age = age;
+            City = city;
+            Count = count;
+        }
+        
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public int Count { get; }
+
+        public int CompareTo(Person other)
+        {
+            int result = Name.CompareTo(other.Name);
+            if (result==0)
+            {
+                result = this.Age.CompareTo(other.Age);
+            }
+            if (result == 0)
+            {
+                result = this.City.CompareTo(other.City);
+
+            }
+
+            return result;
+
+        }
+
+       
+        
+    }
+}
---
+++ 18.IteratorsAndComparatorsExercise/05.ComparingObjects/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.ComparingObjects
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Person> citizenList = new List<Person>();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                if (input[0]=="END")
+                {
+                    break;
+                }
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                string city = input[2];
+
+                Person citizen = new Person(name, age, city);
+                citizenList.Add(citizen);
+
+            }
+
+            int index = int.Parse(Console.ReadLine())-1;
+
+            int equals = 0;
+            int different = 0;
+
+            foreach (Person item in citizenList)
+            {
+                if (citizenList[index].CompareTo(item) ==0)
+                {
+                    equals++;
+                }
+                else
+                {
+                    different++;
+                }
+                
+            }
+
+            if (equals<=1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{equals} {different} {citizenList.Count}");
+                
+            }
+        }
+    }
+}
