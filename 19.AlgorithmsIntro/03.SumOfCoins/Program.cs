8e67d7698b089d262a05920c5ee724a98c8ac6d0
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-15 23:06:56 +0000

    Add files via upload

---
+++ 19.AlgorithmsIntro/03.SumOfCoins/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace SumOfCoins
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            int[] coins = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int targetSum = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> result = ChooseCoins(coins, targetSum);
+
+
+            //int totalNumbersCoinsNeeded = 0;
+            //foreach (var item in result)
+            //{
+            //    totalNumbersCoinsNeeded += item.Value;
+            //}
+            //
+            //Console.WriteLine($"Number of coins to take: {totalNumbersCoinsNeeded}"); 
+            Console.WriteLine($"Number of coins to take: {result.Sum(x => x.Value)}");
+            foreach (var item in result.OrderByDescending(x => x.Key))
+            {
+                Console.WriteLine($"{item.Value} coin(s) with value {item.Key}");
+            }
+        }
+
+
+        public static Dictionary<int, int> ChooseCoins(IList<int> coins, int targetSum)
+        {
+            coins = coins.OrderBy(x => x).ToList();
+            Dictionary<int, int> coinsCount = new Dictionary<int, int>();
+            int index = coins.Count - 1;
+
+            while (index > -1)
+            {
+                int currentCoin = coins[index];
+
+                int result = targetSum / currentCoin;
+
+                if (result < 1)
+                {
+                    index--;
+                    continue;
+                }
+
+
+                coinsCount.Add(currentCoin, result);
+
+                targetSum -= currentCoin * result;
+
+                if (targetSum == 0)
+                {
+                    break;
+                }
+
+            }
+            if (targetSum > 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return coinsCount;
+        }
+    }
+}
