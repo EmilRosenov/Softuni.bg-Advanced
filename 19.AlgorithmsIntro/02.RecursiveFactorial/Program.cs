79922d6d35d17c0741ed56cece66cbfd7e1b940e
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-15 21:03:46 +0000

    Add files via upload

---
+++ 19.AlgorithmsIntro/02.RecursiveFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02.RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int factorial = GetFactorial(n);
+            Console.WriteLine(factorial);
+        }
+
+        private static int GetFactorial(int n)
+        {
+            if (n==0)
+            {
+                return 1;
+            }
+            int result = n * GetFactorial(n - 1);
+            return result;
+        }
+    }
+}
