ee75462fc1c316a8dad332345be6da5f5fa4c4d7
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-15 20:37:52 +0000

    Add files via upload

---
+++ 19.AlgorithmsIntro/01.RecursiveArraySum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.RecursiveArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = SumArray(integers,0);
+            Console.WriteLine(sum);
+        }
+
+        private static int SumArray(int[] integers,int index)
+        {
+            if (index==integers.Length)
+            {
+                return 0;
+            }
+            return integers[index] + SumArray(integers, index + 1);
+        }
+    }
+}
