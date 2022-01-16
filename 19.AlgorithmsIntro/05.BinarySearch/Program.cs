609caa82149fb928d488f05363cb2a5387a9047b
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-16 19:05:19 +0000

    Add files via upload

---
+++ 19.AlgorithmsIntro/05.BinarySearch/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int numberToSearch = int.Parse(Console.ReadLine());
+
+            int index = BinarySearch(array, numberToSearch);
+            Console.WriteLine(index);
+            
+        }
+
+        private static int BinarySearch(int[] array, int numberToSearch)
+        {
+            int low = 0;
+            int high = array.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+
+                if (numberToSearch > array[mid])
+                {
+                    low = mid + 1;
+                }
+                else if (numberToSearch < array[mid])
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
