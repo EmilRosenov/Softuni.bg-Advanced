746768e54e0388e964b74d18369c03c30e3180a7
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-17 21:47:19 +0000

    Add files via upload

---
+++ 20.Exams/01.C#AdvancedExam-28June2020/01.Bombs/01.AdvancedExam28June2020/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.BOmbs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> bombEffect = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToList();
+            List<int> bombCasing = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToList();
+
+            int daturaBombsSum = 40;
+            int cherryBombsSum = 60;
+            int smokeDecoyBombsSum = 120;
+
+            int daturaBombCount = 0;
+            int cherryBombsCount = 0;
+            int smokeDecoyBombsCount = 0;
+            bool ifRemoved = false;
+
+            for (int i = 0; i <= bombEffect.Count; i++)
+            {
+               if (bombEffect.Count == 0 || bombCasing.Count == 0)
+               {
+                   Console.WriteLine("You don't have enough materials to fill the bomb pouch.");
+                   if (bombEffect.Count == 0)
+                   {
+                       Console.WriteLine("Bomb Effects: empty");
+                   }
+                   else
+                   {
+                       Console.WriteLine($"Bomb Effects: {string.Join(", ", bombEffect)}");
+                   }
+                   if (bombCasing.Count == 0)
+                   {
+                       Console.WriteLine("Bomb Casings: empty");
+                   }
+                   else
+                   {
+                       Console.WriteLine($"Bomb Casings: {string.Join(", ", bombCasing)}");
+                   }
+
+                   Console.WriteLine($"Cherry Bombs: {cherryBombsCount}");
+                   Console.WriteLine($"Datura Bombs: {daturaBombCount}");
+                   Console.WriteLine($"Smoke Decoy Bombs: {smokeDecoyBombsCount}");
+                   return;
+               }
+               if (cherryBombsCount >=3 && daturaBombCount>=3 && smokeDecoyBombsCount>=3)
+               {
+                   Console.WriteLine("Bene! You have successfully filled the bomb pouch!");
+                   Console.WriteLine($"Bomb Effects: {string.Join(", ", bombEffect)}");
+                   Console.WriteLine($"Bomb Casings: {string.Join(", ", bombCasing)}");
+
+                   Console.WriteLine($"Cherry Bombs: {cherryBombsCount}");
+                   Console.WriteLine($"Datura Bombs: {daturaBombCount}");
+                   Console.WriteLine($"Smoke Decoy Bombs: {smokeDecoyBombsCount}");
+                   return;
+               }
+                if (ifRemoved && i!=0)
+                {
+                    i--;
+                }
+
+                for (int j = bombCasing.Count - 1; j >= 0; j--)
+                {
+                    
+                    if (bombEffect[i] + bombCasing[j] == daturaBombsSum)
+                    {
+                        daturaBombCount++;
+                        bombEffect.RemoveAt(i);
+                        bombCasing.RemoveAt(j);
+                        ifRemoved = true;
+                        break;
+                    }
+                   
+                    else if (bombEffect[i] + bombCasing[j] == cherryBombsSum)
+                    {
+                        cherryBombsCount++;
+                        bombEffect.RemoveAt(i);
+                        bombCasing.RemoveAt(j);
+                        ifRemoved = true;
+                        break;
+                    }
+                    else if (bombEffect[i] + bombCasing[j] == smokeDecoyBombsSum)
+                    {
+                        smokeDecoyBombsCount++;
+                        bombEffect.RemoveAt(i);
+                        bombCasing.RemoveAt(j);
+                        ifRemoved = true;
+                        break;
+                    }
+
+                    else if (bombEffect[i] + bombCasing[j] != daturaBombsSum &&
+                            bombEffect[i] + bombCasing[j] != cherryBombsCount &&
+                            bombEffect[i] + bombCasing[j] != smokeDecoyBombsCount)
+                    {
+
+                        bombCasing[j] -= 5;
+                        j++;
+                    }
+
+                }
+            }
+
+            if (bombEffect.Count == 0 || bombCasing.Count == 0)
+            {
+                Console.WriteLine("You don't have enough materials to fill the bomb pouch.");
+                if (bombEffect.Count == 0)
+                {
+                    Console.WriteLine("Bomb Effects: empty");
+                }
+                else
+                {
+                    Console.WriteLine($"Bomb Effects: {string.Join(", ", bombEffect)}");
+                }
+                if (bombCasing.Count == 0)
+                {
+                    Console.WriteLine("Bomb Casings: empty");
+                }
+                else
+                {
+                    Console.WriteLine($"Bomb Casings: {string.Join(", ", bombCasing)}");
+                }
+
+                Console.WriteLine($"Cherry Bombs: {cherryBombsCount}");
+                Console.WriteLine($"Datura Bombs: {daturaBombCount}");
+                Console.WriteLine($"Smoke Decoy Bombs: {smokeDecoyBombsCount}");
+                //return;
+            }
+            if (cherryBombsCount >= 3 && daturaBombCount >= 3 && smokeDecoyBombsCount >= 3)
+            {
+                Console.WriteLine("Bene! You have successfully filled the bomb pouch!");
+                Console.WriteLine($"Bomb Effects: {string.Join(", ", bombEffect)}");
+                Console.WriteLine($"Bomb Casings: {string.Join(", ", bombCasing)}");
+
+                Console.WriteLine($"Cherry Bombs: {cherryBombsCount}");
+                Console.WriteLine($"Datura Bombs: {daturaBombCount}");
+                Console.WriteLine($"Smoke Decoy Bombs: {smokeDecoyBombsCount}");
+                //return;
+            }
+        }
+    }
+}
---
+++ 20.Exams/01.C#AdvancedExam-28June2020/01.Bombs/02.Snake/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Snake
+{
+    public class Program
+    {
+        public int foodQuantity;
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+            int snakeRow = -1;
+            int snakeCol = -1;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string line = Console.ReadLine();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = line[col];
+
+                    if (matrix[row,col]=='S')
+                    {
+                        snakeRow = row;
+                        snakeCol = col;
+                    }
+                }
+            }
+
+            //PrintMatrix(matrix);
+            IsInside(matrix, n, n);
+           
+            
+            int foodQuantity = 0;
+            
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                Move(command,matrix,snakeRow,snakeCol);
+
+                if (IsInside(matrix, snakeRow, snakeCol) == false || foodQuantity >= 10)
+                {
+                    break;
+                }
+
+                
+            }
+
+
+            if (!IsInside(matrix, n, n))
+            {
+                Console.WriteLine("Game over!");
+                Console.WriteLine($"Food eaten: {foodQuantity}");
+                PrintMatrix(matrix);
+            }
+            else if (foodQuantity >= 10)
+            {
+                Console.WriteLine("You won! You fed the snake.");
+                Console.WriteLine($"Food eaten: {foodQuantity}");
+                PrintMatrix(matrix);
+            }
+
+        }
+
+        public static void Move(string command,char[,]matrix, int snakeRow, int snakeCol)
+        {
+            
+            if (command== "left")
+            {
+                matrix[snakeRow, snakeCol] = '.';
+                if (IsFood(matrix, snakeRow, snakeCol))
+                {
+
+                    matrix[snakeRow, snakeCol - 1] = 'S';
+                    snakeCol -= 1;
+                }
+
+            }
+            else if (command == "right")
+            {
+                matrix[snakeRow, snakeCol] = '.';
+                if (matrix[snakeRow, snakeCol + 1] == '*')
+                {
+                    foodQuantity++;
+                    matrix[snakeRow, snakeCol + 1] = 'S';
+                }
+               
+            }
+            else if (command == "up")
+            {
+                matrix[snakeRow, snakeCol] = '.';
+                if (matrix[snakeRow-1, snakeCol] == '*')
+                {
+                    foodQuantity++;
+                    matrix[snakeRow-1, snakeCol] = 'S';
+                }
+               
+            }
+            else if (command == "down")
+            {
+                matrix[snakeRow, snakeCol] = '.';
+                if (matrix[snakeRow +1, snakeCol] == '*')
+                {
+                    foodQuantity++;
+                    matrix[snakeRow +1, snakeCol] = 'S';
+                }
+            }
+        }
+
+        private static bool IsFood(char[,] matrix, int snakeRow, int snakeCol)
+        {
+            return matrix[snakeRow, snakeCol - 1] == '*';
+        }
+
+        public static bool IsInside(char[,]matrix,int row, int col)
+        {
+            if (row < matrix.GetLength(0) && row >= 0 
+                && col <matrix.GetLength(1) && col >=0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row,col]);
+                }
+                Console.WriteLine();
+            }
+        }
+        
+    }
+}
