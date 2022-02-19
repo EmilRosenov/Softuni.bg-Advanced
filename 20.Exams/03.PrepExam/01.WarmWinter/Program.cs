eef155cf51251144da8a5be8d2bce1076d372447
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-02-19 18:05:15 +0000

    Add files via upload

---
+++ 20.Exams/03.PrepExam/01.WarmWinter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.WarmWinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] secondInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> hats = new Stack<int>(firstInput);
+            Queue<int> scarves = new Queue<int>(secondInput);
+            List<int> priceList = new List<int>();
+            
+            while (hats.Count>0 && scarves.Count>0)
+            {
+                int hat = hats.Peek();
+                int scarf = scarves.Peek();
+                if (hat > scarf)
+                {
+                    int price = hat + scarf;
+                    priceList.Add(price);
+
+                    hats.Pop();
+                    scarves.Dequeue();
+                }
+                else if (scarf > hat)
+                {
+                    hats.Pop();
+                }
+                else if (scarf == hat)
+                {
+                    scarves.Dequeue();
+                    int incrementation = hats.Pop()+1;
+                    hats.Push(incrementation);
+
+                }
+            }
+            Console.WriteLine($"The most expensive set is: {priceList.Max()}");
+            Console.WriteLine(string.Join(" ", priceList));
+        }
+    }
+}
