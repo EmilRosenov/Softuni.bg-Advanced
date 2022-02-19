c533c67ee4c6080c4c4c7cb0e3528e9ce53bda3b
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-02-19 22:20:10 +0000

    Add files via upload

---
+++ 20.Exams/03.PrepExam/VetClinic/Clinic.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        public List<Pet> data;
+
+        public Clinic(int capacity)
+        {
+            data = new List<Pet>();
+            Capacity = capacity;
+            
+        }
+
+        public int Capacity { get; set; }
+        
+        public int Count => data.Count;
+
+        public void Add(Pet pet)
+        {
+            if (data.Count<Capacity)
+            {
+                data.Add(pet);
+            }
+        }
+        public bool Remove(string name)
+        {
+            Pet pet = data.FirstOrDefault(x => x.Name == name);
+            return data.Remove(pet);
+            
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            var pet = data.FirstOrDefault(x => x.Name == name && x.Owner == owner);
+
+           
+            return pet;
+        }
+        public Pet GetOldestPet()
+        {
+            Pet pet = data.OrderByDescending(x => x.Age).FirstOrDefault();
+            return pet;
+
+
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("The clinic has the following patients:");
+            foreach (var pet in data)
+            {
+                sb.AppendLine($"Pet {pet.Name} with owner: {pet.Owner}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
